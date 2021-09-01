7256af4e66c8a2e5baf07b360aa485d6364ac185
swanf <zj.pan@trip.com>
2021-09-02 00:34:12 +0800

    update fix for scrollbar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -114,8 +114,6 @@ export default () => {
       className="container"
       style={{
         display: `flex`,
-        width: `100%`,
-        height: `100vh`,
         background: `orange`,
       }}
     >
